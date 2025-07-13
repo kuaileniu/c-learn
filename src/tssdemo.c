c54f9f17fe1fbd3695b1faa31da2b9fde91409b8
vagrant <hggfjt@126.com>
2025-07-13 13:20:36 +0000

    f

--- src/tssdemo.c
+++ src/tssdemo.c
@@ -12,17 +12,25 @@ void destructor(void *value)
     printf("Freed.\n");
 }
 
-int thrd_proc(void *arg)
+static void do_print(void)
 {
+    printf("%s.\n", (char *)tss_get(key));
+    thrd_sleep(&(struct timespec){2, 0}, 0);
+}
+
+static int thrd_proc(void *arg)
+{
+    // printf("Key: %s .\n", (char );
     tss_set(key, malloc(strlen((char *)arg) + 10));
     strcpy((char *)tss_get(key), "Hello, ");
-    strcat((char*)tss_get(key),(char*)arg);
+    strcat((char *)tss_get(key), (char *)arg);
+    do_print();
     return 0;
 }
 
 void run_tssdemo(void)
 {
-    printf("55555Running TSS Demo...\n");
+    printf("Running TSS Demo...\n");
     tss_create(&key, destructor);
 
     thrd_t t1, t2;
